46d368d0a0184a91209111a8a9356f6555facb6d
DESKTOP-1KHIUD2\viki <s107213042@mail1.ncnu.edu.tw>
2020-11-19 15:22:51 +0800

    Teacher

---
+++ TeacherAddForm.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require("dbconnect.php");
+$id = (int)$_GET['id'];
+$sql = "select * from student where id = $id;";
+$result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message.");
+$rs=mysqli_fetch_assoc($result);
+if (! $rs) {
+	echo "no data found";
+	exit(0);
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+<body>
+<h1>Add New Content</h1>
+<form method="post" action="TeacherAdd.php">
+
+	  <input type='hidden' name='id' value='<?php echo $id ?>'>
+
+      description: <input name="Tcontent" type="text" id="Tcontent" value="<?php echo htmlspecialchars($rs['Tcontent']);?>" /> <br>
+
+      sign: <input name="Tsign" type="int" id="Tsign" value="<?php echo htmlspecialchars($rs['Tsign']);?>" /> <br>
+
+      <input type="submit" name="Submit" value="送出" />
+	</form>
+  </tr>
+</table>
+</body>
+</html>
---
+++ TeacherList.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+require("dbconnect.php");
+
+$sql = "select * from student where status = 0 ";
+$result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message.");
+$jobStatus = array('未完成','已完成','已結案','已取消');
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body>
+
+<p>Teacher List !! </p>
+<hr />
+導師視訪說明
+<table width="200" border="1">
+  <tr>
+    <td>id</td>
+    <td>name</td>
+    <td>type</td>
+	<td>father</td>
+    <td>mother</td>
+	<td>status</td>
+	<td>Tcontent</td>
+	<td>Tsign</td>
+	<td>-</td>
+  </tr>
+<?php
+
+while (	$rs=mysqli_fetch_assoc($result)) {
+
+	echo "<tr><td>" . $rs['id'] . "</td>";
+	echo "<td>{$rs['name']}</td>";
+	echo "<td>" , htmlspecialchars($rs['type']), "</td>";
+	echo "<td>" , htmlspecialchars($rs['father']), "</td>";
+	echo "<td>" , htmlspecialchars($rs['mother']), "</td>";
+	echo "<td>{$jobStatus[$rs['status']]}</td>" ;
+	echo "<td>" , $rs['Tcontent'], "</td>";
+	echo "<td>" , $rs['Tsign'], "</td>";
+	echo "<td>" ;
+	echo "<a href='TeacherAddForm.php?id={$rs['id']}'> Add </a>";
+	echo "</td></tr>";
+}
+?>
+</table>
+</body>
+</html>
